63a9fa5e01679c52f67a8c950115780ec65ea8f1
Alexis FILLION <afillion@e3r2p3.42.fr>
2015-12-18 18:04:18 +0100

    mofid a re tester

--- get_next_line.c
+++ get_next_line.c
@@ -1,68 +1,28 @@
 #include "get_next_line.h"
 
-static char		*ft_realloc(char **save, size_t n)
+int					get_next_line(int fd, char **line)
 {
-	char	*s;
+	static char		*mem;
+	char			buff[BUFF_SIZE + 1];
+	int				ret;
 
-	s = ft_strnew(n);
-	if (s)
-		ft_strncpy(s, *save, n);
-	ft_strdel(save);
-	return (s);
-}
-
-int				get_next_line(int const fd, char **line)
-{
-	char	buf[BUFF_SIZE + 1] = {0};
-	int		ret;
-	int		total;
-	int		previous;
-	char	*tmp;
-
-	*line = NULL;
-	previous = -1;
-	total = 0;
-	if (fd == -1)
+	ret = 1;
+	if (BUFF_SIZE <= 0 || !line)
 		return (-1);
-	if (fd == previous)
+	if (!mem)
+		mem = ft_strnew(0);
+	while (!(ft_strchr(mem, '\n')) && (ret = read(fd, buff, BUFF_SIZE)))
 	{
-		*line = ft_realloc(line, BUFF_SIZE);
-		ft_strncpy(*line, buf, BUFF_SIZE);
-		if ((tmp = ft_strchr(*line, '\n')) || (tmp = ft_strchr(*line, -1)))
-		{
-			*tmp = '\0';
-			ft_strncpy(buf, tmp + 1, BUFF_SIZE);
-			previous = fd;
-			return (1);
-		}
-		previous = fd;
-		return (0);
+		if (ret == -1)
+			return (-1);
+		buff[ret] = 0;
+		mem = ft_strjoin(mem, buff);
 	}
-	while ((ret = read(fd, line, BUFF_SIZE) > 0))
-	{
-		*line = ft_realloc(line, total + ret);
-		ft_strncpy(*line + total, buf, ret);
-		if ((tmp = ft_strchr(*line, '\n')) || (tmp = ft_strchr(*line, -1)))
-		{
-			*tmp = '\0';
-			ft_strncpy(buf, tmp + 1, BUFF_SIZE);
-			return (1);
-		}
-		total += ret;
-	}
-	return (0);
-}
-
-#include <stdlib.h>
-#include <fcntl.h>
-
-int		main(int ac, char **av)
-{
-	int		fd;
-	char	**line;
-
-	line = NULL;
-	fd = open(av[1], O_RDONLY);
-	get_next_line(fd, line);
-	return (0);
+	if (ft_strchr(mem, '\n') || ((*line = ft_strdup(mem)) && 0))
+		*line = ft_strsub(mem, 0, ft_strchr(mem, '\n') - mem + 1);
+	if (ret)
+		line[0][ft_strlen(*line) - 1] = 0;
+	mem = ft_strsub(mem, ft_strchr(mem, '\n') - mem + 1, \
+			ft_strlen(ft_strchr(mem, '\n')));
+	return (ret == 0 ? 0 : 1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,10 +1,11 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 32
+# define BUFF_SIZE 8
 # include "libft/libft.h"
 # include <sys/types.h>
 # include <sys/uio.h>
 # include <unistd.h>
+# include <stdlib.h>
 
 int		get_next_line(int const fd, char **line);
 
